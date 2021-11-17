d7b7bf372152aa6c5684f8461f15d189d4521546
nirzaf <mfmfazrin1986@gmail.com>
2021-11-18 01:13:52 +0530

    message producer

--- RabbitMQProducer/Program.cs
+++ RabbitMQProducer/Program.cs
@@ -4,7 +4,7 @@ using RabbitMQ.Client;
 using System.Text;
 using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Message Producer Running");
 
 var factory = new ConnectionFactory { Uri= 
     new Uri("amqp://guest:guest@localhost:5672") 
