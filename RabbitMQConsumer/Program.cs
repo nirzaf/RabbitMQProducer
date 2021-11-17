e397c3e802d9408e5dc8258fa8634bda0e021201
nirzaf <mfmfazrin1986@gmail.com>
2021-11-18 00:55:41 +0530

    commit changes

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory
+{
+    Uri =
+    new Uri("amqp://guest:guest@localhost:15672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("Demo-Queue", true, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, e) => { 
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+
+
+
+//var message = new { name = "Producer", message = "Hello" };
+//var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+channel.BasicConsume("Demo-queue", true, consumer);
--- RabbitMQProducer/Program.cs
+++ RabbitMQProducer/Program.cs
@@ -7,12 +7,12 @@ using System.Text.Json;
 Console.WriteLine("Hello, World!");
 
 var factory = new ConnectionFactory { Uri= 
-    new Uri("amqp://guest:guest@localhost:5672") 
+    new Uri("amqp://guest:guest@localhost:15672") 
     };
 
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
-channel.QueueDeclare("Demo Queue", true, false, false, null);
+channel.QueueDeclare("Demo-Queue", true, false, false, null);
 var message = new { name = "Producer", message = "Hello" };
 var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
 
